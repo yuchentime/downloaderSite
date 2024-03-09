c9f21140612f8dc4e069e961c6221dac454e94d4
yuchentime <yuchentime@gmail.com>
2024-03-09 17:49:34 +0800

    友链

--- app/page.jsx
+++ app/page.jsx
@@ -12,6 +12,10 @@ export default function Home() {
           </div>
           <div className="navbar-end hidden lg:flex text-sm">
             友链：
+            <a target="_blank" href="https://answerbook.oksocool.com/" className="text-white cursor-pointer">
+              答案之书
+            </a>
+            、
             <a target="_blank" href="https://www.itdoc666.com/" className="text-white cursor-pointer">
               知识充电站-专业的资料分享平台
             </a>
